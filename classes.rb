b1f5f62a0c31648518218cdecaf38dd2913fdf77
Gregor Cox <gregorcox@gmail.com>
2018-05-28 15:53:31 +0100

    added setters

--- classes.rb
+++ classes.rb
@@ -50,6 +50,17 @@ class Student
     return @cohort
   end
 
+  def set_name(new_name)
+    @name = new_name
+  end
+
+  def set_cohort(new_cohort)
+    @cohort = new_cohort
+  end
+  
+
+
+
 
 
 
--- specs/classes_spec.rb
+++ specs/classes_spec.rb
@@ -12,7 +12,18 @@ class StudentTest < MiniTest::Test
     student = Student.new("Greg", "E22")
     assert_equal("E22", student.cohort)
   end
-  
+
+  def test_set_name
+    student = Student.new("Greg", "E22")
+    student.set_name("Tim")
+    assert_equal("Tim", student.name())
+  end
+
+  def test_set_cohort
+    student = Student.new("Greg", "E22")
+    student.set_cohort("E10")
+    assert_equal("E10", student.cohort())
+  end
 
 
 end
